4d5f6ce09fe2bea88d1e22160772968ac20d6ec4
tech-911 <babatundeolatunji74@gmail.com>
2022-02-27 23:22:11 +0100

    updating home page with order

--- src/pages/customer/index.js
+++ src/pages/customer/index.js
@@ -5,7 +5,7 @@ import { FaUserAlt } from "react-icons/fa";
 import { SiGooglemessages } from "react-icons/si";
 import { RiLogoutCircleFill } from "react-icons/ri";
 import Logo1 from "../../Assets/image/Logo2.png";
-import { Route, Routes } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 const home = <AiFillHome />;
 const profile = <FaUserAlt />;
@@ -14,24 +14,24 @@ const logout = <RiLogoutCircleFill />;
 
 const sidebarContent = [
   {
-    link: "/admin/home",
+    link: "/Customer/Home",
     icon: home,
     title: "Home",
   },
   {
-    link: "/admin/monitor",
+    link: "/Customer/Profile",
     icon: profile,
     title: "Profile",
   },
 
   {
-    link: "/admin/create-users",
+    link: "/Customer/Blog",
     icon: blog,
     title: "Blog",
   },
 
   {
-    link: "/logout",
+    link: "/Customer/Logout",
     icon: logout,
     title: "Logout",
   },
@@ -49,7 +49,7 @@ const Index = () => {
     <div>
       {/* -----------------------------Header begins------------------------------ */}
       <div
-        className={`w-full rounded-b-[41px] p-6 bg-[#D7EBE2] flex items-center justify-between fixed top-0  ${
+        className={`w-full rounded-b-[41px] p-6 bg-[#D7EBE2] flex items-center justify-between fixed top-0 z-10 ${
           hamburger ? "blur-sm" : "blur-none"
         }`}
       >
@@ -59,11 +59,10 @@ const Index = () => {
           }}
           className="text-[#0E4E48] font-bold text-[24px] cursor-pointer"
         />
-        <span className="text-[#0E4E48] font-bold text-[24px] font-['Roboto']">
-        
-          {pathname.includes("/Customer/home") && "Home"}
-          {pathname.includes("/Customer/profile") && "Profile"}
-          {pathname.includes("/Customer/blog") && "Blog"}
+        <span className="text-[#0E4E48] font-bold text-[24px] font-['Roboto'] ml-[45px]">
+          {pathname.includes("/Customer/Home") && "Home"}
+          {pathname.includes("/Customer/Profile") && "Profile"}
+          {pathname.includes("/Customer/Blog") && "Blog"}
         </span>
         <img src={Logo1} alt="logo" className="h-[84px] w-[83px]" />
       </div>
@@ -72,7 +71,7 @@ const Index = () => {
 
       {/* ----------------------------------sidebar beigns------------------------------------ */}
       <div
-        className={`h-full w-1/2 col-span-9 pl-5 bg-[#E8E8E8] z-10 shadow-lg shadow-[#807b7b] rounded-r-[20px] fixed top-0 left-0 ${
+        className={`h-full w-1/2 pl-5 bg-[#E8E8E8] z-20 shadow-lg shadow-[#807b7b] rounded-r-[20px] fixed top-0 left-0 ${
           hamburger ? " visible " : " hidden "
         }`}
       >
@@ -90,7 +89,7 @@ const Index = () => {
               key={idx}
               className={` flex text-base font-medium my-3 hover:bg-[#0E4E48] hover:text-white  rounded-l-md hover:shadow-sm hover:shadow-[#000000] p-2 ${
                 pathname.includes(item.link)
-                  ? "bg-#0E4E48 text-white "
+                  ? "bg-[#0E4E48] text-white "
                   : "text-gray-500"
               }`}
             >
@@ -105,29 +104,17 @@ const Index = () => {
       </div>
       {/* --------------Router begins------------------ */}
       <div
+        onClick={() => {
+          handleHamburger(0);
+        }}
         className={`${
           hamburger ? "blur-sm" : "blur-none"
-        } h-screen overflow-y-scroll col-span-3 bg-ejaice-form-bg pb-10 mt-40`}
+        } h-screen pt-[150px] px-12 z-0`}
       >
-        dsgff
-        ipsiumipsiumipsiumipsiumipsiumipsiumipsiumipsiumipsiumipsiumipsiumipsiumipsiumipsiumipsiumipsiumipsiumipsiumipsiumipsiumipsiumipsiumipsiumipsiumipsiumipsiumipsiumipsiumipsiumipsiumipsium
-        <Routes>
-          <Route path="/Customer/cxvfvzx" element={null} />
-          <Route path="/Customer/Czcxvxvv" element={null} />
-          <Route path="/Customer/zxcvxv" element={null} />
-          <Route path="/Customer/zxcv" element={null} />
-        </Routes>
+        <Outlet />
       </div>
     </div>
   );
 };
 
 export default Index;
-
-
-//  {
-//    activeTab === 0 && <Riders />;
-//  }
-//  {
-//    activeTab === 1 && <Agents />;
-//  }
